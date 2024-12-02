49b0a7bd68061f67e647ea51bd3a8660291d0fd4
asd <asd>
2024-12-02 11:52:02 +0100

    vami

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-console.log("Hello");
-function asd(text) {
+function addDiv(text) {
   const asd = document.createElement("div");
   asd.textContent = text;
   document.body.appendChild(asd);
 }
-async function get() {
-  url = "https://pokeapi.co/api/v2/pokemon/";
-  const risponse = await fetch(url);
-  console.log(risponse);
-  const dataasd = await risponse.json();
-  for (let index = 0; index < dataasd.results.length; index++) {
-    asd(dataasd.results[index].name);
-  }
-
-  console.log(dataasd.results);
+function addImg(alt, src) {
+  const asd = document.createElement("img");
+  asd.arc = src;
+  asd.alt = alt;
+  document.body.appendChild(asd);
+  document.body.appendChild(document.createElement("img"));
 }
-asd("Alma");
-asd("Korte");
-get();
+async function getPokemonList(url) {
+  const risponse = await fetch(url);
+  const data = await risponse.json();
+
+  console.log(data.results);
+  for (let index = 0; index < data.results.length; index++) {
+    addDiv(data.results[index].name);
+    const poke_response = await fetch(data.results[index].url);
+    const poke_data = await poke_response.json();
+    console.log(poke_data.sprites.front_default);
+    addImg(data.results[index].name, poke_data.sprites.front_default);
+  }
+}
+
+getPokemonList("https://pokeapi.co/api/v2/pokemon/");
